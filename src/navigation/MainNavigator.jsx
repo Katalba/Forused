ca05fdd9216620fc2bc2ce52d0189b1a21decfbc
katalba <katherine.albanicg@gmail.com>
2023-10-13 18:04:18 -0300

    desafio complete

--- src/navigation/MainNavigator.jsx
+++ src/navigation/MainNavigator.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { setCameraImage, setUser } from '../features/auth/authSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
 import AuthStackNavigator from './AuthStackNavigator'
 import BottomTabNavigator from './BottomTabNavigator'
-import { useSelector } from 'react-redux'
+import { fetchSession } from '../db'
+import { useGetProfileImageQuery } from '../services/shopApi'
 
 const MainNavigator = () => {
-  const user = useSelector(state => state.auth.user)
+  // const [user, setuser] = useState(null)
+  const { user, localId } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
+  const { data } = useGetProfileImageQuery(localId)
+
+  useEffect(() => {
+    console.log('Main navigator data', data)
+    if (data) {
+      dispatch(setCameraImage(data.image))
+    }
+  }, [data])
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const session = await fetchSession()
+        console.log('Esta es la sesion', session)
+        if (session.rows.length) {
+          console.log(session.rows._array[0])
+          const user = session.rows._array[0]
+          dispatch(setUser(user))
+        }
+      } catch (error) {
+        console.log('Error en obtener ususario', error.message)
+      }
+    })()
+  }, [])
 
   return user ? <BottomTabNavigator /> : <AuthStackNavigator />
 }
